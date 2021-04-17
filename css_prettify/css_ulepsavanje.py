8ace6aeec13cfcfd78ade04f0d3b1c424eafde1c
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-04-17 19:48:11 +0200

    Add files via upload

--- css_prettify/css_ulepsavanje.py
+++ css_prettify/css_ulepsavanje.py
@@ -4,36 +4,43 @@ import sublime_plugin
 
 class css_ulepsavanje(sublime_plugin.TextCommand):
 	maks_duzina = 0
-	
-	def run(self, edit):
-		#self.view.sel().clear()
-		
-		r      = sublime.Region(0, self.view.size() - 1)
-		s1     = self.view.substr(r)
-		
-		redovi = s1.split("\n")
-		tokeni = []
-		
-		self.rastavljanje_tokena(redovi, tokeni)
-		self.racunaje_duzina(tokeni)
 
-		s2 = self.ispis_tokena(tokeni)
-		
-		self.view.replace(edit, r, s2)
+	def run(self, edit):
+
+		selekcije = self.view.sel()
+		for s in selekcije:
+			if s.empty():
+				r = sublime.Region(0, self.view.size() - 1)
+			else:
+				r = sublime.Region(s.begin(), s.end())
+
+			s1     = self.view.substr(r)
+
+			redovi = s1.split("\n")
+			tokeni = []
+
+			self.rastavljanje_tokena(redovi, tokeni)
+			self.racunaje_duzina(tokeni)
+
+			s2     = self.ispis_tokena(tokeni)
+
+			self.view.replace(edit, r, s2)
 
 #-------------------------------------------------------------------------------
 
 	def rastavljanje_tokena(self, redovi, tokeni):
 		for r in redovi:
-			if r != "":
-				t = r.strip()
-				if t.startswith("}") or t.endswith("{"):
+			t = r.rstrip()
+			if t.endswith(";"):
+				if t.lstrip().startswith("//") or t.lstrip().startswith("@"):
 					tokeni.append([False, t])
 				else:
-					p = t.split(": ")
-					a = p[0].strip()
+					p = t.split(":")
+					a = p[0].rstrip()
 					v = p[1].strip()
 					tokeni.append([True, a, v, len(a)])
+			else:
+				tokeni.append([False, t])
 
 #-------------------------------------------------------------------------------
 
@@ -45,6 +52,8 @@ class css_ulepsavanje(sublime_plugin.TextCommand):
 				if t[3] > self.maks_duzina:
 					self.maks_duzina = t[3]
 
+		self.maks_duzina = self.maks_duzina - 1
+
 #-------------------------------------------------------------------------------
 
 	def ispis_tokena(self, tokeni):
@@ -52,17 +61,16 @@ class css_ulepsavanje(sublime_plugin.TextCommand):
 
 		for t in tokeni:
 			if t[0] == True:
-				s = s + "\t" + str(t[1]) + ": " + self.dodavanje_spejsova(self.maks_duzina - t[3])  + str(t[2]) + "\n"
+				s = s + str(t[1]) + ": " + self.dodavanje_spejsova(self.maks_duzina - t[3])  + str(t[2]) + "\n"
 			else:
 				s = s + str(t[1]) + "\n"
-				if t[1] == "}":
-					s = s + "\n"
-		return s
+		return s[ 0 : len(s)-1 ]
 
 #-------------------------------------------------------------------------------
 
 	def dodavanje_spejsova(self, n):
 		s = ""
+		n = n + 1
 
 		for i in range(0, n):
 			s = s + " "
