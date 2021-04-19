26c93fdb804ff2243b71b60584406b2aec0ea730
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-04-19 20:02:19 +0200

    Update idiosync_markdown.py

--- idiosync_markdown/idiosync_markdown.py
+++ idiosync_markdown/idiosync_markdown.py
@@ -10,16 +10,18 @@
 #
 # Sintaksa (donekle se poklapa sa zvaničnom specifikacijom):
 # 
-#     #    - Dodaje "<h1>" i "</h1>" oko reda koji počinje sa "#" i briše tarabe
-#     ##   - Dodaje "<h2>" i "</h2>" oko reda koji počinje sa "##" i briše tarabe
-#     ###  - Dodaje "<h3>" i "</h3>" oko reda koji počinje sa "###" i briše tarabe
-#     #### - Dodaje "<h4>" i "</h4>" oko reda koji počinje sa "####" i briše tarabe
+#     #    - Dodaje "<h1>" i "</h1>" oko reda koji počinje sa "#"
+#     ##   - Dodaje "<h2>" i "</h2>" oko reda koji počinje sa "##"
+#     ###  - Dodaje "<h3>" i "</h3>" oko reda koji počinje sa "###
+#     #### - Dodaje "<h4>" i "</h4>" oko reda koji počinje sa "####"
 #            (h5 i h6 mi, za sada, ne trebaju, ali, prisutan je kod
 #             i za njih, pod komentarom; ako se stavi pet ili
 #             šest zvezdica, skripta će to preppoznati kao h4)
 #     *    - Menja zvezdicu u "<ul>"
 #     **   - Menja dve zvezdice u "</ul>"
 #     \t   - Dodaje "<li>" i "</li>" oko reda koji počinje sa "\t"
+#     ~~   - Komentar - početak / završetak bloka koji će biti direktno
+#            preslikan (znakovi moraju biti na početku reda)
 #
 # Copyright (C) 2021. Nikola Vukićević
 #
@@ -27,6 +29,7 @@
 
 import sublime
 import sublime_plugin
+import re
 
 class idiosync_markdown(sublime_plugin.TextCommand):
 	
@@ -35,6 +38,7 @@ class idiosync_markdown(sublime_plugin.TextCommand):
 		r = sublime.Region(0, self.view.size())
 		s = self.view.substr(r)
 		s = self.idiosyncParse(s)
+		#s = self.zamena_tokena_slike(s)
 			
 		self.view.replace(edit, r, s)
 
@@ -42,12 +46,32 @@ class idiosync_markdown(sublime_plugin.TextCommand):
 		redovi = s.split("\n")
 		tokeni = []
 		s      = ""
+		parse  = True
 
 		for r in redovi:
 			if not r:
 				continue
 			
-			# Za sada mi ne trebaju h5 i h6, ali, nikad se ne zna ....
+			if parse == True:
+				if r.startswith("~~"):
+					parse = False
+					continue
+				self.ucitavanje_tokena(r, tokeni)
+				
+			else:
+				if r.startswith("~~"):
+					parse = True
+					tokeni.append(["", "", ""])
+					continue
+				tokeni.append([r, "", ""])
+
+		for t in tokeni:
+			s = s + t[0] + t[1] + t[2] + "\n"
+
+		return s
+
+	def ucitavanje_tokena(self, r, tokeni):
+		# Za sada mi ne trebaju h5 i h6, ali, ako zatreba ....
 
 			# if r.startswith("######"):
 			# 	tokeni.append(["<h6>", r.lstrip('#').strip(), "</h6>\n"])
@@ -55,37 +79,46 @@ class idiosync_markdown(sublime_plugin.TextCommand):
 			# if r.startswith("#####"):
 			# 	tokeni.append(["<h5>", r.lstrip('#').strip(), "</h5>\n"])
 			# 	continue
-			
-			if r.startswith("!!"):
-				tokeni.append([r.lstrip('!'), "", ""])
-				continue
-			if r.startswith("####"):
-				tokeni.append(["<h4>", r.lstrip('#').strip(), "</h4>\n"])
-				continue
-			if r.startswith("###"):
-				tokeni.append(["<h3>", r.lstrip('#').strip(), "</h3>\n"])
-				continue
-			if r.startswith("##"):
-				tokeni.append(["<h2>", r.lstrip('#').strip(), "</h2>\n"])
-				continue
-			if r.startswith("#"):
-				tokeni.append(["<h1>", r.lstrip('#').strip(), "</h1>\n"])
-				continue
-			if r.startswith("**"):
-				tokeni.append(["</ul>\n", "",        ""])
-				continue
-			if r.startswith("*"):
-				tokeni.append(["<ul>", "",        ""])
-				continue
-			if r.startswith("\t"):
-				tokeni.append(["\t<li>", r.strip(), "</li>"])
-				continue
+		if r.startswith("####"):
+			tokeni.append(["<h4>", r.lstrip('#').strip(), "</h4>\n"])
+			return
+		if r.startswith("###"):
+			tokeni.append(["<h3>", r.lstrip('#').strip(), "</h3>\n"])
+			return
+		if r.startswith("##"):
+			tokeni.append(["<h2>", r.lstrip('#').strip(), "</h2>\n"])
+			return
+		if r.startswith("#"):
+			tokeni.append(["<h1>", r.lstrip('#').strip(), "</h1>\n"])
+			return
+		if r.startswith("**"):
+			tokeni.append(["</ul>\n", "",        ""])
+			return
+		if r.startswith("*"):
+			tokeni.append(["<ul>", "",        ""])
+			return
+		if r.startswith("\t"):
+			tokeni.append(["\t<li>", r.strip(), "</li>"])
+			return
 
-			# Ako red nije ništa 'posebno', onda je <p> :)
+		# Ako red nije ništa 'posebno', onda je <p> :)
+		tokeni.append(["<p>\n\t", r.strip(), "\n</p>\n"])
 
-			tokeni.append(["<p>\n\t", r.strip(), "\n</p>\n"])
+	# --------------------------------------------------
+	# Automatksa numeracija slika u tekstu
+	# Za ovo koristim posebne snipete, što verovatno
+	# ne bi bilo zanimljivo svima, pa se zato metoda
+	# ne poziva po default-u
+	# --------------------------------------------------
 
-		for t in tokeni:
-			s = s + t[0] + t[1] + t[2] + "\n"
+	def zamena_tokena_slike(self, s):
+	    lista = re.split("#s#", s)
 
-		return s
+	    n = ""
+	    i = 1
+
+	    for l in lista:
+	    	n = n + l + str(i)
+	    	i = i + 1
+
+	    return n.rstrip('0123456789\n ') + "\n"
