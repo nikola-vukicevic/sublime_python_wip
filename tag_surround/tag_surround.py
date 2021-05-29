f1b922b745dfe9b346c34e4280f2aba627799d30
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-05-29 09:48:04 +0200

    Add files via upload

--- tag_surround/tag_surround.py
+++ tag_surround/tag_surround.py
@@ -3,17 +3,19 @@ import sublime_plugin
 
 
 class tag_surround(sublime_plugin.TextCommand):
-	
-	def run(self, edit):
 
-		selekcije = self.view.sel()
-		for s in selekcije:
-			if not s.empty():
-				r = sublime.Region(s.begin(), s.end())
+    def run(self, edit):
 
-			s1 = self.view.substr(r)
-			# promeniti u drugi par tagova po potrebi
-			t1 = "<span class='klasa_1'>"
-			t2 = "</span>"
-			
-			self.view.replace(edit, r, t1 + s1 + t2)
+        selekcije = self.view.sel()
+        for s in selekcije:
+            if not s.empty():
+                r = sublime.Region(s.begin(), s.end())
+            else:
+                r = self.view.word(s)
+
+            s1 = self.view.substr(r)
+            # promeniti u drugi par tagova po potrebi
+            t1 = "<code class='kod_u_tekstu'>"
+            t2 = "</code>"
+
+            self.view.replace(edit, r, t1 + s1 + t2)
